0dad06545bf17e3e146257f7ec727a0e4447b397
kavinthangavel <kxvinthxngxvel@gmail.com>
2025-02-26 17:26:58 +0530

    uncommented code

--- boot.sh
+++ boot.sh
@@ -16,8 +16,8 @@ echo -e "$ascii_art"
 echo "=> Atelier is for fresh Ubuntu 24.04+ installations only!"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
-# sudo apt-get update >/dev/null
-# sudo apt-get install -y git >/dev/null
+sudo apt-get update >/dev/null
+sudo apt-get install -y git >/dev/null
 
 echo "Cloning Atelier..."
 rm -rf ~/.local/share/atelier
